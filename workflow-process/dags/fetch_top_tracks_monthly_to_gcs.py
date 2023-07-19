a0eb8439d518cac58114fc340bbf464149b6a23f
slothPete7773 <Veerakit413@gmail.com>
2023-07-19 18:09:26 +0700

    feat: structure monthly fetch

---
+++ workflow-process/dags/fetch_top_tracks_monthly_to_gcs.py
@@ -0,0 +1,53 @@
+import configparser
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils import timezone 
+
+DAGS_FOLDER = "/opt/airflow/dags"
+DATA_FOLDER = "/opt/airflow/data"
+CREDENTIALS_FOLDER = "/opt/airflow/credentials"
+config = configparser.ConfigParser()
+config.read(f'{CREDENTIALS_FOLDER}/env.conf')
+
+# Define constant variables
+# BUSINESS_DOMAIN = 0
+# DATA = 0
+LOCATION = "asia-southeast1"
+PROJECT_ID = "slothpete7773-warehouse"
+GCS_BUCKET = "slothpete-spotify-side-project-playground"
+KEYFILE = config.get('google_service', 'gcs_service_account')
+
+def _extract_data_from_server():
+    # TODO: fetch data
+    # TODO: save raw data as-is to file
+    pass
+    # print(f"KEYFILE: {KEYFILE}")
+    # return 0
+
+def _upload_data_to_gcs():
+    # TODO: read the json file
+    # TODO: upload to GCS
+    pass
+
+default_args = {
+    "owner": "slothPete7773",
+    "start_date": timezone.datetime(2023, 7, 1)
+}
+
+with DAG(
+    dag_id="fetch_top_tracks_monthly",
+    schedule="@daily",
+    default_args=default_args,
+    tags=["test", "fetching"]
+):
+    extract_data_from_server = PythonOperator(
+        task_id="extract_data_from_server",
+        python_callable=_extract_data_from_server
+    )
+    
+    upload_data_to_gcs = PythonOperator(
+        task_id="upload_data_to_gcs",
+        python_callable=_upload_data_to_gcs
+    )
+
+    extract_data_from_server >> upload_data_to_gcs
